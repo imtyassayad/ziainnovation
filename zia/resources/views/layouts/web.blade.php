7ddf746d78cab696fa80dfcc1e055e72b31a5d2a
Zia Innovaton <imtyas.sayad@gmail.com>
2022-09-07 00:28:44 +0530

    added home about service and contact pages

--- zia/resources/views/layouts/web.blade.php
+++ zia/resources/views/layouts/web.blade.php
@@ -12,33 +12,7 @@
 
     <link rel="stylesheet" href="{{asset('web/css/bootstrap.min.css')}}" />
 
-    <link rel="stylesheet" href="{{asset('web/css/animate.css')}}" />
 
-    <link rel="stylesheet" href="{{asset('web/css/jquery-ui.min.css')}}" />
-
-    <link rel="stylesheet" href="{{asset('web/css/meanmenu.min.css')}}" />
-
-    <link rel="stylesheet" href="{{asset('web/css/owl.carousel.css')}}" />
-
-    <link rel="stylesheet" href="{{asset('web/css/font-awesome.min.css')}}" />
-
-    <link rel="stylesheet" href="{{asset('web/css/flaticon.css')}}" />
-
-    <link rel="stylesheet" href="{{asset('web/inc/custom-slider/css/nivo-slider.css')}}" type="text/css" />
-    <link rel="stylesheet" href="{{asset('web/inc/custom-slider/css/preview.css')}}" type="text/css" media="screen" />
-
-    <link rel="stylesheet" href="{{asset('web/css/style.css')}}" />
-
-    <link rel="stylesheet" href="{{asset('web/multicolor-css/skype-color.css')}}" />
-    <link rel="stylesheet" href="{{asset('web/multicolor-css/red-color.css')}}" />
-    <link rel="stylesheet" href="{{asset('web/multicolor-css/green-color.css')}}" />
-    <link rel="stylesheet" href="{{asset('web/multicolor-css/blue-color.css')}}" />
-
-    <link rel="stylesheet" href="{{asset('web/css/responsive.css')}}" />
-
-    <script src="{{asset('web/js/vendor/modernizr-2.8.3.min.js')}}"></script>
-
-    {{-- <script src="../../../../www.digitalcenturysf.com/google_analytics.js')}}"></script> --}}
 </head>
 
 <body>
@@ -331,10 +305,7 @@
 
     <script src="{{asset('web/js/plugins.js')}}"></script>
 
-    <script src="{{asset('web/inc/custom-slider/js/jquery.nivo.slider.js')}}" type="text/javascript"></script>
-    <script src="{{asset('web/inc/custom-slider/home.js')}}" type="text/javascript"></script>
 
-    <script src="{{asset('web/js/main.js')}}"></script>
 </body>
 
 </html>
