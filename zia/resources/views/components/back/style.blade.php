146e7e2853f29c41a6ef096956b9d49dc3255400
Zia Innovaton <72249751+imtyassayad@users.noreply.github.com>
2025-02-09 18:38:40 +0530

    removed laravel app
    removed Laravel app

--- zia/resources/views/components/back/style.blade.php
+++
@@ -1,7 +0,0 @@
-<div>
-     <!-- BEGIN GLOBAL MANDATORY STYLES -->
-     <link href="https://fonts.googleapis.com/css?family=Nunito:400,600,700" rel="stylesheet">
-     <link href="{{asset('back/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet" type="text/css" />
-     <link href="{{asset('back/assets/css/plugins.css')}}" rel="stylesheet" type="text/css" />
-     <!-- END GLOBAL MANDATORY STYLES -->
-</div>
